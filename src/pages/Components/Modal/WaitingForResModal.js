b90472b1da95acf1499fb466b684750c2b2137a9
Prajil K <prajilk20017@gmail.com>
2023-07-25 22:36:45 +0530

    server bootup loading warning added

--- src/pages/Components/Modal/WaitingForResModal.js
+++ src/pages/Components/Modal/WaitingForResModal.js
@@ -4,6 +4,7 @@ function WaitingForResModal() {
 
     return (
         <div className='overlay'>
+            <p className='server-loading'>! The server is hosted on Render.com's free tier, and while it takes a few minutes to load up initially, once it's up and running, everything runs smoothly.</p>
             <div className="waitingForResModal">
                 <div className="modalBody">
                     <div className="spinner-border" role="status">
